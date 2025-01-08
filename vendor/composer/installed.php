21a0dbb5ac76fe97d30ddc5af337a2bde318865a
Muhammad Vafoev <muhammadjonvafoev@gmail.com>
2025-01-08 09:26:37 +0500

    update namespace

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'larataj/xml-helpers',
+        'pretty_version' => '1.0.1',
+        'version' => '1.0.1',
+        'reference' => null,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'larataj/xml-helpers' => array(
+            'pretty_version' => '1.0.1',
+            'version' => '1.0.1',
+            'reference' => null,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
